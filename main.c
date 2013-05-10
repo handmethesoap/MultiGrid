4130bdb0a346503156dd01fa9aa82f5ea2b91e96
handmethesoap <samnees@gmail.com>
2013-05-10 12:45:38 +0200

    minor corrections to make program compile

--- main.c
+++ main.c
@@ -10,17 +10,40 @@
 int main(int argc, char* argv[])
 {
  
-  int l = atoi(argv[1]);
+//   int l = atoi(argv[1]);
+//   
+//   int n = (1 << l) + 1;
+//   
+//   Grid A(n, boundary_function_zero, v_initialiser_function_sin_2, );
+//   for(int i = 0; i< 50; ++i)
+//   {
+//     A.rb_gauss_seidel_relaxation();
+//   }
+//   A.print_v();
+  //std::ofstream myfile;
+  //myfile.open("");
   
-  int n = (1 << l) + 1;
+  Grid* A;
+  double initial_error;
+  double current_error;
   
-  Grid A(n, boundary_function_zero);
-  for(int i = 0; i< 50; ++i)
+  for( int n = 7; n <= 102; ++n)
   {
-    A.rb_gauss_seidel_relaxation();
+    A = new Grid(66, boundary_function_zero, v_initialiser_function_sin_2);
+    initial_error = A->calculate_L_inf_norm(v_initialiser_function_zero);
+    current_error = initial_error;
+    
+    while( 100*current_error > initial_error )
+    {
+      A->jacobi_relaxation();
+    }
+    //myfile << n-2 << " " << A->calculate_L_inf_norm(exact_solution)<< std::endl;
+    
+    delete A;
   }
-  A.print_v();
   
+  //myfile.close();
+  return 0;
 
 }
 
